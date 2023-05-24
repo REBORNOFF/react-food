321daf183c06c67ca29a68ea5cf99784d11661c7
Danilov Nikolai <mailabc.ru@yandex.ru>
2023-05-24 21:00:36 +0300

    init: Сreating a project

---
+++ src/layout/header/Header.jsx
@@ -0,0 +1,26 @@
+const Header = () => {
+	return (
+		<nav className='red darken-4'>
+			<div className='container'>
+				<div className='nav-wrapper'>
+					<a href='/' className='brand-logo'>
+						React Shop
+					</a>
+					<ul id='nav-mobile' className='right hide-on-med-and-down'>
+						<li>
+							<a
+								href='https://github.com/REBORNOFF/react-shop-practice'
+								target='_blank'
+								rel='noreferrer'
+							>
+								GitHub
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Header;
